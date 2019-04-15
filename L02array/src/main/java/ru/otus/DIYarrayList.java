8c451afb090678796767102bd128c9f469e6676d
anastasia <aaremez@mail.ru>
2019-04-15 08:08:19 +0300

    -

--- L02array/src/main/java/ru/otus/DIYarrayList.java
+++ L02array/src/main/java/ru/otus/DIYarrayList.java
@@ -5,42 +5,72 @@ import java.util.*;
 class DIYarrayList<T> implements List<T> {
     private final int INIT_SIZE = 10;
     private int size = 0;
-    T[] ourArray;
+    Object[] defaultArray = {};
+    Object[] ourArray;
 
-    public static void main() {
-        List<Integer> example = new ArrayList<Integer>();
-        example.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21));
-
-        for (int i = 0; i < example.size(); i++) {
-            System.out.println(example.get(i));
-        }
-
+    public static void main(String[] args) {
+        //First
         List<Integer> my = new DIYarrayList<Integer>();
-        my.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22));
+        List<Integer> desc = new DIYarrayList<Integer>();
+        my.addAll(Arrays.asList(8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22));
+        my.add(101);
+        //Collections.addAll(my,58, 65, 233, 33);
+        //Collections.copy(desc,my);
+        //my.addAll(Arrays.asList(56, 65, 233, 33));
         for (int i = 0; i < my.size(); i++) {
-            System.out.println(my.get(i));
+            System.out.print(my.get(i) + " ");
         }
 
-//        Collections.static <T> void copy(List<? super T> dest, List<? extends T> src)
-//        Collections.static <T> void sort(List<T> list, Comparator<? super T> c)
+        //Second
+
+//        System.out.printf("Copy:");
+//        for (int i = 0; i < desc.size(); i++) {
+//            System.out.print(my.get(i) + " ");
+//        }
+
+        //Third
+//        Collections.sort(my, new Comparator<Integer>() {
+//            public int compare(Integer o1, Integer o2) {
+//                //return o1.toString().compareTo(o2.toString());
+//                return o1.compareTo(o2);
+//            }
+//        });
+//        System.out.println("\nSorted:");
+//        for (int i = 0; i < my.size(); i++) {
+//            System.out.print(my.get(i) + " ");
+//        }
+
+    }
+
+    public void sort(Comparator<? super T> c)
+    {
+        Arrays.sort(ourArray, 0, size, (Comparator<? super Object>) c);
     }
 
     public DIYarrayList() {
-
-        T[] init_object = null;
-        this.ourArray = init_object;
+        this.ourArray = defaultArray;
     }
 
+    public void copy(DIYarrayList<? super T> dest)
+    {
+        dest.addAll(this);
+    }
 
     public boolean addAll(Collection<? extends T> c) {
 
-        size = size + c.size();
-        System.arraycopy(c.toArray(), 0, ourArray, size, c.size());
+        Object[] newArray = c.toArray();
+        int newArraySize = newArray.length;
+        Object[]  ourNewArray = new Object[size + newArraySize];
+        System.arraycopy(ourArray, 0, ourNewArray, 0 , ourArray.length);
+        System.arraycopy(newArray, 0, ourNewArray, size , newArraySize);
+        ourArray = new Object[size + newArraySize];
+        System.arraycopy(ourNewArray,0,ourArray,0,ourNewArray.length);
+        this.size = size + newArraySize;
         return c.size() != 0;
     }
 
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     public boolean isEmpty() {
@@ -64,7 +94,17 @@ class DIYarrayList<T> implements List<T> {
     }
 
     public boolean add(T t) {
-        throw new UnsupportedOperationException();
+        Object[] newArray = new Object[size +1];
+        newArray[size] = t;
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+        //System.arraycopy(newArray,0,ourArray,0,newArray.length);
+        ourArray = new Object[size + 1];
+
+        System.arraycopy(newArray,0,ourArray,0,newArray.length-1);
+        size++;
+        return true;
     }
 
     public boolean remove(Object o) {
@@ -93,12 +133,13 @@ class DIYarrayList<T> implements List<T> {
     }
 
     public T get(int index) {
-        return ourArray[index];
+        return (T)ourArray[index];
 
     }
 
     public T set(int index, T element) {
-        throw new UnsupportedOperationException();
+        this.ourArray[index] = element;
+        return element;
     }
 
     public void add(int index, T element) {
