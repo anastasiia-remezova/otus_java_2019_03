b348d7a0197e7b23ced13d9e2805056081b45b83
anastasia <aaremez@mail.ru>
2019-04-28 15:01:17 +0300

    -

--- L02array/src/main/java/ru/otus/DIYarrayList.java
+++ L02array/src/main/java/ru/otus/DIYarrayList.java
@@ -11,21 +11,40 @@ class DIYarrayList<T> implements List<T> {
     public static void main(String[] args) {
         //First
         List<Integer> my = new DIYarrayList<Integer>();
-        List<Integer> desc = new DIYarrayList<Integer>();
+        List<Integer> dest = new DIYarrayList<Integer>();
 
+
+        //first
         my.addAll(Arrays.asList(8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22));
         my.add(101);
-        Collections.addAll(my,58, 65, 233, 33);
-        //Collections.copy(desc,my);
+        Collections.addAll(my, 58, 65, 233, 33);
+
         my.addAll(Arrays.asList(56, 65, 233, 33));
         for (int i = 0; i < my.size(); i++) {
             System.out.println(my.get(i) + " ");
         }
 
+        //Second
+        List<Integer> my1 = new ArrayList<Integer>();
+        my1.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22));
+
+        List<Integer> destArray = new ArrayList<Integer>(my1.size());
+        Collections.copy(destArray, my);
+
+        System.out.println("\ncopy ArrayList:");
+        System.out.println(destArray.size());
+        for (int i = 0; i < destArray.size(); i++) {
+            System.out.println(destArray.get(i) + " ");
+        }
+        System.out.println("\ncopy DIYarrayList:");
+        System.out.println(destArray.size());
+        Collections.copy(dest, my);
+
+
+
         //Third
         Collections.sort(my, new Comparator<Integer>() {
             public int compare(Integer o1, Integer o2) {
-                //return o1.toString().compareTo(o2.toString());
                 return o1.compareTo(o2);
             }
         });
@@ -36,8 +55,7 @@ class DIYarrayList<T> implements List<T> {
 
     }
 
-    public void sort(Comparator<? super T> c)
-    {
+    public void sort(Comparator<? super T> c) {
         Arrays.sort(ourArray, 0, size, (Comparator<? super Object>) c);
     }
 
@@ -45,8 +63,7 @@ class DIYarrayList<T> implements List<T> {
         this.ourArray = defaultArray;
     }
 
-    public void copy(DIYarrayList<? super T> dest)
-    {
+    public void copy(DIYarrayList<? super T> dest) {
 //        System.out.println("sizes:" + dest.size() + this.size);
 //        dest.size = this.size;
 //        dest.ourArray = new Object[this.size];
@@ -57,11 +74,11 @@ class DIYarrayList<T> implements List<T> {
 
         Object[] newArray = c.toArray();
         int newArraySize = newArray.length;
-        Object[]  ourNewArray = new Object[size + newArraySize];
-        System.arraycopy(ourArray, 0, ourNewArray, 0 , ourArray.length);
-        System.arraycopy(newArray, 0, ourNewArray, size , newArraySize);
+        Object[] ourNewArray = new Object[size + newArraySize];
+        System.arraycopy(ourArray, 0, ourNewArray, 0, ourArray.length);
+        System.arraycopy(newArray, 0, ourNewArray, size, newArraySize);
         ourArray = new Object[size + newArraySize];
-        System.arraycopy(ourNewArray,0,ourArray,0,ourNewArray.length);
+        System.arraycopy(ourNewArray, 0, ourArray, 0, ourNewArray.length);
         this.size = size + newArraySize;
         return c.size() != 0;
     }
@@ -91,11 +108,11 @@ class DIYarrayList<T> implements List<T> {
     }
 
     public boolean add(T t) {
-        Object[] newArray = new Object[size +1];
-        System.arraycopy(ourArray,0,newArray,0,size);
+        Object[] newArray = new Object[size + 1];
+        System.arraycopy(ourArray, 0, newArray, 0, size);
         newArray[size] = t;
         ourArray = new Object[size + 1];
-        System.arraycopy(newArray,0,ourArray,0,newArray.length);
+        System.arraycopy(newArray, 0, ourArray, 0, newArray.length);
         size++;
         return true;
     }
@@ -126,7 +143,7 @@ class DIYarrayList<T> implements List<T> {
     }
 
     public T get(int index) {
-        return (T)ourArray[index];
+        return (T) ourArray[index];
 
     }
 
