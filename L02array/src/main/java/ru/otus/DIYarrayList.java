4fc8327d662b8da29605332c1aec6cc67b247dda
anastasia <aaremez@mail.ru>
2019-05-03 22:13:13 +0300

    -

--- L02array/src/main/java/ru/otus/DIYarrayList.java
+++ L02array/src/main/java/ru/otus/DIYarrayList.java
@@ -5,55 +5,8 @@ import java.util.*;
 class DIYarrayList<T> implements List<T> {
     private final int INIT_SIZE = 10;
     private int size = 0;
-    Object[] defaultArray = {};
-    Object[] ourArray;
-
-    public static void main(String[] args) {
-        //First
-        List<Integer> my = new DIYarrayList<Integer>();
-        List<Integer> dest = new DIYarrayList<Integer>();
-
-
-        //first
-        my.addAll(Arrays.asList(8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22));
-        my.add(101);
-        Collections.addAll(my, 58, 65, 233, 33);
-
-        my.addAll(Arrays.asList(56, 65, 233, 33));
-        for (int i = 0; i < my.size(); i++) {
-            System.out.println(my.get(i) + " ");
-        }
-
-        //Second
-        List<Integer> my1 = new ArrayList<Integer>();
-        my1.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22));
-
-        List<Integer> destArray = new ArrayList<Integer>();
-        Collections.copy(destArray, my);
-
-        System.out.println("\ncopy ArrayList:");
-        System.out.println(destArray.size());
-        for (int i = 0; i < destArray.size(); i++) {
-            System.out.println(destArray.get(i) + " ");
-        }
-        System.out.println("\ncopy DIYarrayList:");
-        System.out.println(destArray.size());
-        //Collections.copy(dest, my);
-
-
-
-        //Third
-        Collections.sort(my, new Comparator<Integer>() {
-            public int compare(Integer o1, Integer o2) {
-                return o1.compareTo(o2);
-            }
-        });
-        System.out.println("\nSorted:");
-        for (int i = 0; i < my.size(); i++) {
-            System.out.print(my.get(i) + " ");
-        }
-
-    }
+    private final static Object[] defaultArray = {};
+    private Object[] ourArray;
 
     public void sort(Comparator<? super T> c) {
         Arrays.sort(ourArray, 0, size, (Comparator<? super Object>) c);
@@ -64,9 +17,6 @@ class DIYarrayList<T> implements List<T> {
     }
 
     public void copy(DIYarrayList<? super T> dest) {
-//        System.out.println("sizes:" + dest.size() + this.size);
-//        dest.size = this.size;
-//        dest.ourArray = new Object[this.size];
         dest.addAll(this);
     }
 
@@ -170,6 +120,7 @@ class DIYarrayList<T> implements List<T> {
 
     public ListIterator<T> listIterator() {
         throw new UnsupportedOperationException();
+        //return new DIYListIterator();
     }
 
     public ListIterator<T> listIterator(int index) {
@@ -179,4 +130,8 @@ class DIYarrayList<T> implements List<T> {
     public List<T> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
+
+
+
+
 }
---
+++ L02array/src/main/java/ru/otus/testDIYarrayList.java
@@ -0,0 +1,65 @@
+package ru.otus;
+
+import java.util.*;
+
+public class testDIYarrayList {
+
+    public static void main(String[] args) {
+        //First
+        List<Integer> my = new DIYarrayList<Integer>();
+        List<Integer> dest = new DIYarrayList<Integer>();
+
+
+        //first
+        my.addAll(Arrays.asList(8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22));
+        my.add(101);
+        Collections.addAll(my, 58, 65, 233, 33);
+
+        my.addAll(Arrays.asList(56, 65, 233, 33));
+        for (int i = 0; i < my.size(); i++) {
+            System.out.println(my.get(i) + " ");
+        }
+
+        //Second
+        List<Integer> my1 = new ArrayList<Integer>();
+        my1.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9,10,11));
+
+        List<Integer> destArray = new ArrayList<Integer>();
+        destArray.addAll(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0));
+        Collections.copy(destArray, my1);
+
+        System.out.println("\ncopy ArrayList:");
+        System.out.println("copy size:" + destArray.size());
+        for (int i = 0; i < destArray.size(); i++) {
+            System.out.println(destArray.get(i) + " ");
+        }
+
+
+        List<Integer> my2 = new DIYarrayList<Integer>();
+        my2.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9,10,11));
+
+        List<Integer> destArray2 = new ArrayList<Integer>();
+        destArray2.addAll(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0));
+
+        Collections.copy(destArray2, my2);
+
+        System.out.println("\ncopy ArrayList:");
+        System.out.println("copy size:" + destArray.size());
+        for (int i = 0; i < destArray.size(); i++) {
+            System.out.println(destArray.get(i) + " ");
+        }
+
+
+        //Third
+        Collections.sort(my, new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        System.out.println("\nSorted:");
+        for (int i = 0; i < my.size(); i++) {
+            System.out.print(my.get(i) + " ");
+        }
+
+    }
+}
