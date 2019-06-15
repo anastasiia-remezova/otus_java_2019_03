16041ee7e6c05a4d9be5d1b15d8b19985a844d12
anastasia <aaremez@mail.ru>
2019-06-15 16:22:00 +0300

    -

--- L04log/src/main/java/ru/otus/Demo.java
+++ L04log/src/main/java/ru/otus/Demo.java
@@ -1,6 +1,13 @@
 package ru.otus;
 
 class Demo {
+
+    public static void main(String[] args)
+    {
+        Demo d = new Demo();
+        d.action();
+    }
+
     public void action() {
         new TestLogging().calculation(6); }
 }
