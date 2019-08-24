3de01f80dfc1b2709104a30a3cafc01e454889d6
anastasia <aaremez@mail.ru>
2019-08-24 15:05:54 +0300

    -

--- L15ser/src/test/java/ru/otus/TestNjson.java
+++ L15ser/src/test/java/ru/otus/TestNjson.java
@@ -16,17 +16,17 @@ public class TestNjson {
 
     @BeforeAll
     public static void init() throws Exception {
-        Pit = new Programmer("Pit", 21, Arrays.asList("Java"));
-        n = new Njson();
-        json = n.toJson(Pit);
-        System.out.println("Old Pit: " + n.toJson(Pit));
+
     }
 
     @Test
     public void testToJson() throws Exception {
+        Pit = new Programmer("Pit", 21, Arrays.asList("Java"));
+        n = new Njson();
+        json = n.toJson(Pit);
+        System.out.println("Old Pit: " + n.toJson(Pit));
         Gson gson = new Gson();
         Programmer newPit = gson.fromJson(json, Programmer.class);
-        System.out.println("New Pit: " + n.toJson(newPit));
         assertTrue(newPit.equals(Pit));
     }
 
